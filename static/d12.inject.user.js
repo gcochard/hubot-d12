e5fbce9cac1fa1b9d452484cac8c6e964e5b8baf
Greg Cochard <gcochard@linkedin.com>
2016-02-01 19:15:59 -0800

    Fix mixed content warning

--- static/d12.inject.user.js
+++ static/d12.inject.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         D12 turn checker for slack injector
-// @namespace    http://hubot-gregcochard.rhcloud.com/hubot
-// @updateURL    http://hubot-gregcochard.rhcloud.com/hubot/d12.inject.user.js
+// @namespace    https://hubot-gregcochard.rhcloud.com/hubot
+// @updateURL    https://hubot-gregcochard.rhcloud.com/hubot/d12.inject.user.js
 // @version      2.0.0
 // @description  injects the real script so we can debug with firebug
 // @author       Greg Cochard
@@ -15,9 +15,9 @@
 // @match        https://www.dominating12.com/index.php/game/*
 // @grant        none
 // ==/UserScript==
-/*global Ext: false*/
+/*eslint-env browser*/
 
-var e = document.createElement("script");
-e.src = "http://hubot-gregcochard.rhcloud.com/hubot/d12.user.js";
-e.type = "text/javascript";
-document.getElementsByTagName("head")[0].appendChild(e);
+var e = document.createElement('script');
+e.src = 'https://hubot-gregcochard.rhcloud.com/hubot/d12.user.js';
+e.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(e);
