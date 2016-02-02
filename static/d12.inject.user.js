c8fb87638b6b9455cee9643d35cd68a620385c72
Greg Cochard <gcochard@linkedin.com>
2016-02-01 19:12:19 -0800

    Fix inject script permissions

--- static/d12.inject.user.js
+++ static/d12.inject.user.js
@@ -2,13 +2,17 @@
 // @name         D12 turn checker for slack injector
 // @namespace    http://hubot-gregcochard.rhcloud.com/hubot
 // @updateURL    http://hubot-gregcochard.rhcloud.com/hubot/d12.inject.user.js
-// @version      1.0.0
+// @version      2.0.0
 // @description  injects the real script so we can debug with firebug
 // @author       Greg Cochard
-// @match        http://dominating12.com/?cmd=game&sec=play&id=*
-// @match        http://dominating12.com/index.php?cmd=game&sec=play&id=*
-// @match        http://www.dominating12.com/?cmd=game&sec=play&id=*
-// @match        http://www.dominating12.com/index.php?cmd=game&sec=play&id=*
+// @match        http://dominating12.com/game/*
+// @match        http://www.dominating12.com/game/*
+// @match        https://dominating12.com/game/*
+// @match        https://www.dominating12.com/game/*
+// @match        http://dominating12.com/index.php/game/*
+// @match        http://www.dominating12.com/index.php/game/*
+// @match        https://dominating12.com/index.php/game/*
+// @match        https://www.dominating12.com/index.php/game/*
 // @grant        none
 // ==/UserScript==
 /*global Ext: false*/
