50372597c19a6f756c96e431ed50bc2d9c34f3b7
Greg Cochard <greg.cochard@gmail.com>
2017-03-23 08:34:46 -0700

    Add rules engine

--- scripts/inifier.js
+++ scripts/inifier.js
@@ -1,20 +1,32 @@
 // Description:
 //   replace in with :in:
+//   replace g with :goog:
+//   replace 100 with :100:
 
 /*eslint-env node*/
 'use strict';
 
-var _ = require('lodash');
-var util = require('util');
+const percentage = .01;
+const replacements = [ 'in', ['g','goog'], '100' ];
 
 module.exports = function(robot){
-    robot.hear(/.*[^:]in[^:].*/, function(msg){
+  // todo: Make this one function that hears all patterns and replaces all instances
+  replacements.forEach(function(r){
+    var v = r;
+    if(r instanceof Array){
+      r = r[0];
+      v = v[1];
+    }
+    const hear = new RegExp(`.*(?!:)${r}(?!:).*`), re = new RegExp(`(.?)${r}(.?)`,'g'), rep = `$1:${v}:$2`;
+    robot.hear(hear, function(msg){
         if(/Pssst! I didn.t unfurl /.test(msg)){
             return;
         }
-        if(Math.random() < .9){
+        if(Math.random() > percentage){
             return;
         }
-        return msg.reply(`did you mean "${msg.match[0].replace(/([^:])in([^:])/g, '$1:in:$2')}"?`);
+        var replaced = msg.match[0].replace(re, rep);
+        return msg.reply(`did you mean "${replaced}"?`);
     });
+  });
 };
